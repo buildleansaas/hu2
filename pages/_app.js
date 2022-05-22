97d30297d0328efe8b50de2d979dee2a0b2e11b6
Austin Witherow <au.witherow@gmail.com>
2022-05-21 22:24:02 -0400

    add google analytics 4 tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7NT3166Q01"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-7NT3166Q01');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
